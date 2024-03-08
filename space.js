9f0ef4645de21cd111103cd18f08af201da7957e
Pavan khalyan <pavankhalyan2004@gmail.com>
2024-03-08 22:55:32 +0530

    space

---
+++ space.js
@@ -0,0 +1,48 @@
+//function callbackfunction(){
+//   console.log("im a callback function");
+//}
+
+//function higherorderfunction(func){
+//   console.log("i am a higher order function");
+//    func();
+//}
+
+//higherorderfunction(callbackfunction);
+
+//const arr =[1,2,3,4,5];
+//const even =[]
+
+//for(var i of arr){
+//  if(i%2==0){
+//  even.push(i);
+//  }
+//}
+
+//console.log(even);
+
+//const(arr)=[1,2,3,4,5];
+//const even =arr.filter(function((i))
+//if(i%2 ==0){
+//return(i)
+//})
+
+//console.log(even);
+
+//const arr=[1,2,3,4,5];
+
+//const even = arr.filter((i)=> i % 2==0)
+//console.log(even);
+
+//const companies= [
+//    {name: "Company One", category: "Finance", start: 1981, end: 2004},
+//    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+//    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+//    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+//    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+//    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+//    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+//    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+//    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+//  ];
+
+
